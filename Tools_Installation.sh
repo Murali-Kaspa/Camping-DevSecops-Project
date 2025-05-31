2299c99bebb533f2aa9c9d48302bf003c7a720e5
EC2 Default User <ec2-user@ip-172-31-38-168.ap-south-1.compute.internal>
2025-05-31 04:09:44 +0000

    Added Script for installing tools

---
+++ Tools_Installation.sh
@@ -0,0 +1,54 @@
+################################################
+# Created This Script : Murali Krishna Kaspa    #
+# Date : 30-May-2025			       #
+# Script : Packages_Installation		       #	
+################################################
+
+
+#!/bin/bash
+
+#GITHUB
+sudo yum install git -y 
+
+#Jenkins
+sudo yum install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo service jenkins start
+
+#Terraform
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+#Docker
+
+sudo yum install docker -y
+
+#Sonarube_Docker_Image
+docker run -itd --name sonar -p 9000:9000 sonarqube:lts-community
+
+
+sleep 60
+
+echo "Displaying Versions"
+
+
+git_version=$(git --version)
+jenkins_version=$(jenkins --version)
+docker_version=$(docker --version)
+trivy_version=$(trivy --version)
+terraform_version=$(terraform version | head -n 1)
+
+echo "-------------------------------"
+echo "| Tool Name  | Version        |"
+echo "-------------------------------"
+echo "| Git        | $git_version   |"
+echo "| Jenkins    | $jenkins_version |"
+echo "| Docker     | $docker_version |"
+echo "| Trivy      | $trivy_version |"
+echo "| Terraform  | $terraform_version |"
+echo "-------------------------------"
+
+
